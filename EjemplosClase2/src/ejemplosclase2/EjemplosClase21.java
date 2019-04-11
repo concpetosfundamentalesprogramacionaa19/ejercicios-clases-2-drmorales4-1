e2a0c4ff85126f653b5ccb45e55868c455a95077
drmorales4 <davidmoralesec@gmail.com>
2019-04-11 10:45:06 -0500

    Ejercicios en clase-Java

---
+++ EjemplosClase2/src/ejemplosclase2/EjemplosClase21.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemplosclase2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author DAVID
+ */
+public class EjemplosClase21 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        // Este metodo es el principal de mi proyecto
+        // hay q dejar un espacio para cada comentario
+        
+        String nombre;
+        String apellido;
+        Scanner entrada = new Scanner(System.in);
+        
+        System.out.println("Por favor Ingrese su Nombres:");
+        nombre = entrada.nextLine();
+        
+        System.out.println("Por favor Ingrese su Apellidos: ");
+        apellido = entrada.nextLine();
+        
+        System.out.println("\nSu nombre: "+nombre+"\n\n\tSu apellido es: "
+                +apellido);
+        
+    }
+    
+}
